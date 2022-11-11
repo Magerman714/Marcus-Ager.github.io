b929d948aaeb5ece042949cd6ce175b75128d679
Magerman714 <magerman714@gmail.com>
2022-11-11 01:14:20 +0000

    create description of commit

---
+++ studies/variables.js
@@ -0,0 +1,18 @@
+/**
+ * VARIABLES: 
+ * 
+ * 0: Variables are named containers for data in Javascript. To declare variables...
+ * 
+ * 1: Declaration and Assignment
+ *      a. Declaration: Declaration occurs when we declare a variable with a declarative keyword.
+ *      b. Assignment: Assignment occurs when we use the assignment operator to "assign" a value
+ * 
+ * 2: Var, Let, Const
+ *      a: Var
+ *      b: Let
+ *      C: Const 
+ */
+
+// 1 // Declaration
+var age; // declares a variable called age
+console.log(age); // => undefined
